c86f0f03c4acacf4b62b08519f90e934f3ed813c
Fazal E Rabbi <fazalerabbi@Fazals-MacBook-Pro.local>
2022-12-26 18:43:49 +0500

    Resolved Game4 unit tests

--- test/game4Test.js
+++ test/game4Test.js
@@ -6,15 +6,22 @@ describe('Game4', function () {
     const Game = await ethers.getContractFactory('Game4');
     const game = await Game.deploy();
 
-    return { game };
+    const user1 = ethers.provider.getSigner(0);
+    const user2 = ethers.provider.getSigner(1);
+
+
+    return { game, user1, user2 };
   }
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { game, user1, user2 } = await loadFixture(deployContractAndSetVariables);
 
     // nested mappings are rough :}
+    const address1 = await user1.getAddress();
+    const address2 = await user2.getAddress();
 
-    await game.win();
-
+    await game.connect(user1).write(address2);
+    await game.connect(user2).win(address1);
+    
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
   });
